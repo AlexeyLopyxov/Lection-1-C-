586436c081344ca608160f4feec4310102988a23
AlexeyLopuxov <alexeylopyxov00@gmail.com>
2022-11-29 11:23:24 +0500

    Example 3 Sum Random

--- Lection1Sum/Program.cs
+++ Lection1Sum/Program.cs
@@ -1,5 +1,6 @@
-﻿int numA = 3;
-int numB = 51;
+﻿int numA = new Random().Next(1,10); //1,2,3, ... , 9
+int numB = new Random().Next(1,10);
 int result = numA + numB;
 
-Console.WriteLine(result);
+
+Console.WriteLine($"{numA} + {numB} = {result}");
